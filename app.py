be51a0c72c569d1054fa3fb06d27e8d5152cdf49
Satyam6024 <sss.sahusatyam2000@gmail.com>
2025-07-30 23:23:20 +0530

    app ready with html and css - ready to deploy

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
+app=Flask(__name__, static_folder='static')
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
+regmodel=pickle.load(open('mlp_model.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
